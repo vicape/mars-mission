01dfd7a7b6b3da55309c132db6404933f9267ed4
vicape <vic@spaceclub.com.ar>
2024-06-15 23:01:28 -0300

    Actualiza proyecto con registro de intentos de inicio de sesión y conexión a Supabase

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const axios = require('axios');
+const logLoginAttempt = require('./log');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Middleware para obtener la IP del cliente y procesar datos JSON
+app.use((req, res, next) => {
+  req.clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  next();
+});
+app.use(bodyParser.json());
 
 app.get('/api', (req, res) => {
   res.send('Welcome to the Mars Mission API');
@@ -22,6 +31,19 @@ app.get('/api/users', async (req, res) => {
   } catch (error) {
     console.error('Error fetching the CSV file:', error);
     res.status(500).json({ error: 'Error fetching the CSV file' });
+  }
+});
+
+// Endpoint para el intento de inicio de sesión
+app.post('/api/login', async (req, res) => {
+  const { username, password } = req.body;
+  const ip = req.clientIp;
+
+  try {
+    await logLoginAttempt(username, password, ip);
+    res.status(200).json({ message: 'Login attempt logged' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error logging login attempt' });
   }
 });
 
---
+++ api/log.js
@@ -0,0 +1,25 @@
+const { createClient } = require('@supabase/supabase-js');
+
+const supabaseUrl = 'https://yayrbsvafizrldmkxtvj.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlheXJic3ZhZml6cmxkbWt4dHZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg0MTU0NDUsImV4cCI6MjAzMzk5MTQ0NX0.alW7sPzJLaJA_V9Ou4H7QtVotfpJQY9xqIplpr7gN4Q';
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+// Función para registrar el intento de inicio de sesión
+const logLoginAttempt = async (user, pass, ip) => {
+  const time = new Date().toISOString();
+
+  const { data, error } = await supabase
+    .from('log')
+    .insert([
+      { user, pass, time, ip }
+    ]);
+
+  if (error) {
+    console.error('Error logging login attempt:', error);
+  } else {
+    console.log('Login attempt logged successfully:', data);
+  }
+};
+
+module.exports = logLoginAttempt;
