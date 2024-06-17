7ef411f5393edeeb3332e33b7c09b3bb890087f8
vicape <vic@spaceclub.com.ar>
2024-06-16 21:28:04 -0300

    Ajustar campos y agregar validaciones

--- api/log.js
+++ api/log.js
@@ -1,38 +1,18 @@
-const fetch = require('node-fetch');
+const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = 'https://yayrbsvafizrldmkxtvj.supabase.co/rest/v1/log';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlheXJic3ZhZml6cmxkbWt4dHZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg0MTU0NDUsImV4cCI6MjAzMzk5MTQ0NX0.alW7sPzJLaJA_V9Ou4H7QtVotfpJQY9xqIplpr7gN4Q';
+const supabaseUrl = 'https://yayrbsvafizrldmkxtvj.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlheXJic3ZhZml6cmxkbWt4dHZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg0MTQ0NX0.alW7sPzJLaJA_V9Ou4H7QtVotfpJQY9xqIplpr7gN4Q';
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const logLoginAttempt = async (username, password) => {
-  try {
-    const response = await fetch(supabaseUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'apikey': supabaseKey,
-        'Authorization': `Bearer ${supabaseKey}`
-      },
-      body: JSON.stringify({
-        username: username,
-        pass: password,
-        time: new Date().toISOString(),
-        ip: '127.0.0.1'
-      })
-    });
+  const { data, error } = await supabase
+    .from('log')
+    .insert([{ username, pass: password, time: new Date().toISOString(), ip: '127.0.0.1' }]);
 
-    if (!response.ok) {
-      const errorText = await response.text();
-      throw new Error(`Fetch error: ${errorText}`);
-    }
-
-    const data = await response.json();
-    console.log('Data inserted successfully:', data);
-    return data;
-
-  } catch (error) {
-    console.error('Fetch error:', error);
+  if (error) {
     throw error;
   }
+  return data;
 };
 
 module.exports = logLoginAttempt;
