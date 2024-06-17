88102ea394a758e4389a1dcf47aaf57d8340a9d8
vicape <vic@spaceclub.com.ar>
2024-06-16 21:17:11 -0300

    Ajustar campos y agregar validaciones

--- api/index.js
+++ api/index.js
@@ -1,21 +1,30 @@
-const express = require('express');
-const logLoginAttempt = require('./log');
+const { createClient } = require('@supabase/supabase-js');
 
-const app = express();
-app.use(express.json());
+const supabaseUrl = 'https://yayrbsvafizrldmkxtvj.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlheXJic3ZhZml6cmxkbWt4dHZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg0MTU0NDUsImV4cCI6MjAzMzk5MTQ0NX0.alW7sPzJLaJA_V9Ou4H7QtVotfpJQY9xqIplpr7gN4Q';
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-app.post('/api/login', async (req, res) => {
-  const { username, password } = req.body;
-  try {
-    const result = await logLoginAttempt(username, password);
-    res.status(200).json({ message: 'Login logged', result });
-  } catch (error) {
-    console.error('Error logging login attempt:', error);
-    res.status(500).json({ error: 'Error logging login attempt' });
+module.exports = async (req, res) => {
+  if (req.method !== 'POST') {
+    res.status(405).send({ error: 'Method not allowed' });
+    return;
   }
-});
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  const { username, password, time, ip } = req.body;
+
+  try {
+    const { data, error } = await supabase
+      .from('log')
+      .insert([{ username, pass: password, time, ip }]);
+
+    if (error) {
+      console.error('Error logging login attempt:', error);
+      res.status(500).json({ error: error.message });
+    } else {
+      res.status(200).json({ data });
+    }
+  } catch (err) {
+    console.error('Fetch error:', err);
+    res.status(500).json({ error: 'Fetch error' });
+  }
+};
