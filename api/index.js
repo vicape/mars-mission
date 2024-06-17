454bf38042404eb531aabe190e0c666c90591aca
vicape <vic@spaceclub.com.ar>
2024-06-17 20:46:14 -0300

    Agregar importación de createClient en index.js

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const fetch = require('node-fetch');
 const logLoginAttempt = require('./log');
 const ipinfo = require('ipinfo');
+const { createClient } = require('@supabase/supabase-js'); // Asegúrate de importar createClient aquí
 
 const app = express();
 app.use(cors());
