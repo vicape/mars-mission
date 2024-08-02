4c76851d5358dcf33226494fe0e653112eae5dd0
vicape <vic@spaceclub.com.ar>
2024-08-02 16:20:28 -0300

    Ajustar error fetch

--- api/ai.js
+++ api/ai.js
@@ -1,21 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const fetch = require('node-fetch');
-const pdf = require('pdf-parse');
 
 // Cargar la clave API de OpenAI desde las variables de entorno
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-
-// Función para descargar y extraer texto de un PDF desde un URL
-async function extractTextFromPDF(pdfUrl) {
-    const response = await fetch(pdfUrl);
-    const dataBuffer = await response.buffer();
-    const data = await pdf(dataBuffer);
-    return data.text;
-}
-
-// URL del PDF en el almacenamiento en la nube
-const pdfUrl = 'https://your-cloud-storage.com/path_to_your_pdf.pdf';
 
 router.post('/chat', async (req, res) => {
     const { prompt } = req.body;
@@ -23,18 +10,18 @@ router.post('/chat', async (req, res) => {
         return res.status(400).json({ error: 'No prompt provided' });
     }
 
+    const url = 'https://api.openai.com/v1/chat/completions';
+    const data = {
+        model: "gpt-3.5-turbo",  // Asegúrate de usar un modelo disponible
+        messages: [
+            { role: 'system', content: 'Eres un asistente especializado en informar y educar sobre viajes y exploración en Marte. Responde solo con información relacionada con Marte.' },
+            { role: 'user', content: prompt }
+        ]
+    };
+
     try {
-        const marsInformation = await extractTextFromPDF(pdfUrl);
-
-        const url = 'https://api.openai.com/v1/chat/completions';
-        const data = {
-            model: "gpt-3.5-turbo",  // Asegúrate de usar un modelo disponible
-            messages: [
-                { role: 'system', content: marsInformation }, // Agrega información extraída del PDF
-                { role: 'user', content: prompt }
-            ]
-        };
-
+        // Importación dinámica de node-fetch para ES Modules
+        const fetch = (await import('node-fetch')).default;
         const response = await fetch(url, {
             method: 'POST',
             headers: {
