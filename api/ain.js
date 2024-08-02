738ab16ab91a6f5b30f967c0ea02e14299cfccf4
vicape <vic@spaceclub.com.ar>
2024-08-02 04:19:46 -0300

    Ajustar error fetch

--- api/ain.js
+++ api/ain.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const fetch = require('node-fetch');
+//const fetch = require('node-fetch');
 const router = express.Router();
 
 // Asegúrate de tener tu clave API de OpenAI en tu archivo .env
